db2b6c254cbf87d3ca29a5917e8fd82db3da7af3
Joon Yi <26yteoj803a@student.ri.edu.sg>
2026-09-18 23:53:14 +0800

    Update app.py
    Added Flask code for:
    - "home" path (plus redirect to "home" if no path specified)
    - "functional-groups" path - shows all functional groups
    - "functional-groups/<functionalgroup>" path - shows the possible reactions (both to form and from) for the given <functionalgroup>
    - "reagents-conditions" path - shows all the RnC (no data yet)
    - "mechanisms" path - shows all the mechanisms (no data yet)
    
    Added functionality (but no data) for:
    - "reagents-conditions/<reaction> path - shows the starting reagent, RnC, and product of <reaction>
    - "mechanisms/<mechanism> path - shows the detailed mechanism for <mechanism>
    - "qa-tests" path - shows the Inorg. QA cheat sheet plus some additional reactions
    
    To-Do:
    - "POST" method for "search" path
    - "GET" method HTML for "search" path
    - Org QA cheat sheet
    - Populate:
      - "functional-groups/<functionalgroup>"
      - "mechanisms"
      - "mechanisms/<mechanism>
      - "reagents-conditions"
      - "reagents-conditions/<reaction>"
      - "search" database/list

--- app.py
+++ app.py
@@ -1,1 +1,65 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+def linkToFileName(inStr):
+    inList = inStr.strip().split("-")
+    for i in range(len(inList)):
+        inList[i] = inList[i].capitalize()
+
+    outFile = "reactionMap"
+    outFile += " ".join(inList)
+    outFile += ".html"
+    return outFile
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET"])
+def root():
+    return redirect(url_for("home"))
+
+@app.route("/home", methods = ["GET"])
+def home():
+    return render_template("reactionMapHome.html")
+
+@app.route("/functional-groups", methods = ["GET"])
+def functionalGroups():
+    return render_template("reactionMapFunctionalGroups.html")
+
+@app.route("/reagents-conditions", methods = ["GET"])
+def reagentsConditions():
+    return render_template("reactionMapReagentsConditions.html")
+
+@app.route("/mechanisms", methods = ["GET"])
+def mechanisms():
+    return render_template("reactionMapMechanisms.html")
+
+@app.route("/qa-tests", methods = ["GET"])
+def qaTests():
+    return render_template("reactionMapQATests.html")
+
+
+
+@app.route("/functional-groups/<functionalGroup>", methods = ["GET"])
+def getFunctionalGroup(functionalGroup):
+    return render_template(linkToFileName(functionalGroup))
+
+@app.route("/reagents-conditions/<reaction>", methods = ["GET"])
+def getReagentCondition(reaction):
+    return render_template(linkToFileName(reaction))
+
+@app.route("/mechanisms/<mechanism>", methods = ["GET"])
+def getMechanism(mechanism):
+    return render_template(linkToFileName(mechanism))
+
+
+
+@app.route("/search", methods = ["GET", "POST"])
+def search():
+    if request.method == "GET":
+        return render_template("reactionMapSearch.html")
+
+    else:
+        pass
+        
+if __name__ == "__main__":
+    app.run()
 
